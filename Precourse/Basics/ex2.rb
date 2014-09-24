0c031e410d8b1da14d7d5fb27f5341243c2bf1b3
Mohafizz <mohafizz@gmail.com>
2014-09-24 22:39:38 +0800

    Add updates

---
+++ Precourse/Basics/ex2.rb
@@ -0,0 +1,7 @@
+#Use the modulo operator, division, or a combination of both to take a 4 digit number and 
+#find 1) the thousands number 2) the hundreds 3) the tens and 4) and the ones.
+
+puts 9876 / 1000		#displays the thousands
+puts 9876 % 1000 / 100	#displays the hundreds
+puts 9876 % 100 / 10 	#displays the tens
+puts 9876 % 10 			#displays the ones
